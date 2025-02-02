a5f675b5618d99cf8593d1521765e675d00b0c5e
xSE7EN06 <agao6105@gmail.com>
2025-02-02 00:05:18 -0600

    feat(sign-up): agregar nueva pantalla sig-up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'sign-up',
+    loadChildren: () => import('./sign-up/sign-up.module').then( m => m.SignUpPageModule)
+  },
 ];
 
 @NgModule({
