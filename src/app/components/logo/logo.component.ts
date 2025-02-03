8db7ebee826d1eef186fc8192b6b9f8269e9825f
xSE7EN06 <agao6105@gmail.com>
2025-02-03 01:53:46 -0600

    feat: agregar funciones para recibir esa imagen que envia el html

--- src/app/components/logo/logo.component.ts
+++ src/app/components/logo/logo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class LogoComponent  implements OnInit {
+
+  @Input() imagen: string = '';
 
   constructor() { }
 
