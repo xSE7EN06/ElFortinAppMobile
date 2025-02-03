591c07f0de4b0179ae6ffc0f5a61372ec7959220
xSE7EN06 <agao6105@gmail.com>
2025-02-03 01:49:04 -0600

    feat: agregar funciones y validaciones para los inputs en la sección de perfil

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+   form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    })
+  
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  //cramos una funcion para manejar los errores de login.page.html
+  getErrorMessage(controlName: string): string {
+    const control = this.form.get(controlName);
+  
+    //validamos y retornamos
+    if (control?.hasError('required')) {
+      return 'Este campo es obligatorio.';
+    }
+  
+    if (control?.hasError('email')) {
+      return 'Ingresa un correo electrónico válido.';
+    }
+    return ''; // Si no hay errores, retorna un string vacío
   }
 
 }
