6abbd4296f4eeec5aadb3e5d31e2592375676b34
xSE7EN06 <agao6105@gmail.com>
2025-02-06 01:21:56 -0600

    feat: agregar una pagina para el carrito

---
+++ src/app/cart/cart.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.page.html',
+  styleUrls: ['./cart.page.scss'],
+  standalone: false
+})
+export class CartPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
