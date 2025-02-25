97f88971c9922834e341babe9a54d14c88dcbc0e
xSE7EN06 <agao6105@gmail.com>
2025-02-24 23:47:41 -0600

    feat(login): agregar un click en el botón y manejar un loadingProgress

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-login',
@@ -14,7 +17,7 @@ export class LoginPage implements OnInit {
     password: new FormControl('', [Validators.required])
   })
 
-  constructor() { }
+  constructor(private loadingCtrl: LoadingController, private route: Router) { }
 
   ngOnInit() {
   }
@@ -32,5 +35,18 @@ export class LoginPage implements OnInit {
       return 'Ingresa un correo electrónico válido.';
     }
     return ''; // Si no hay errores, retorna un string vacío
+  }
+
+  async login(){
+    const loading = await this.loadingCtrl.create({
+      message: "Iniciando sesión",
+      duration: 2000,
+    });
+
+    await loading.present(); // Espera a que se muestre el loading
+
+    await loading.onDidDismiss(); // Espera a que termine el loading antes de navegar
+  
+    this.route.navigate(['/home']); // Ahora se ejecuta la navegación
   }
 }
