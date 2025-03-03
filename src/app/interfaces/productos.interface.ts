9643cc3b8f7f53eada25c2f2e6a348ce198d50cd
xSE7EN06 <agao6105@gmail.com>
2025-03-03 13:29:56 -0600

    Agregar una carpeta de interfaces para la api

---
+++ src/app/interfaces/productos.interface.ts
@@ -0,0 +1,9 @@
+export interface Producto{
+    id: number;
+    restaurante_id: number;
+    name: string;
+    description: string;
+    img_url: string;
+    price: number;
+    category_id: number;
+}
