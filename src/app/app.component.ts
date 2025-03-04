82126caaa5343b79b0e02cf5e93108bc6d4dc808
xSE7EN06 <agao6105@gmail.com>
2025-03-03 20:33:40 -0600

    feat: agregar funcion StatusBar de capacitor para respetar la barra de estados en los dispositivos fisicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StatusBar, Style} from '@capacitor/status-bar';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,15 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform) {
+    this.initializeApp();
+  }
+
+  // Cada vez que se incia la app, se cargan estos valores
+  initializeApp() {
+    this.platform.ready().then(() => {
+      StatusBar.setOverlaysWebView({ overlay: false }); // evita el toolbar se desplace hasta la barra de estado
+      StatusBar.setBackgroundColor({ color: '#99582a' }); // cambia el color de la barra de estado
+    });
+  }
 }
