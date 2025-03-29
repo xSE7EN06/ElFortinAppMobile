9e3dd47406c0e878c8401ef61acac59535853e2b
xSE7EN06 <agao6105@gmail.com>
2025-03-29 04:19:02 -0600

    chore(interfaces): actualiza interfaz de ordenes

--- src/app/interfaces/ordenes.ts
+++ src/app/interfaces/ordenes.ts
@@ -7,9 +7,11 @@ export interface Ordenes {
     pre_tax_total: string,
     client_id: string,
     payment_method_id: string,
+    payment_method: string,
     post_tax_total: string,
     order_type: string,
     status_id: string,
+    status: string,
     created_at: string,
     discount_id: null,
     deleted_at: null
