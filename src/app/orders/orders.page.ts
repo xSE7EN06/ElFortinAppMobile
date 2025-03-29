dd88a2a2123119c5745d7e518667b402c91f221c
xSE7EN06 <agao6105@gmail.com>
2025-03-29 04:19:43 -0600

    feat(orders): muestra mensajes si no hay pedidos

--- src/app/orders/orders.page.ts
+++ src/app/orders/orders.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdersService } from '../services/order/orders.service';
 import { Ordenes } from '../interfaces/ordenes';
+import { UsuarioService } from '../services/user.service';
 
 @Component({
   selector: 'app-orders',
@@ -12,20 +13,26 @@ export class OrdersPage implements OnInit {
   orders: Ordenes[] = [];
   ordersInProgress: Ordenes[] = [];
   ordersCompleted: Ordenes[] = [];
+  ordersCanceled: Ordenes[] = [];
 
-  constructor(private ordersService: OrdersService) { }
+  selectedSegment: string = 'orderInited';
+
+  constructor(private ordersService: OrdersService, private userService: UsuarioService) { }
 
   ngOnInit() {
     this.loadOrders();
   }
 
-  loadOrders(): void{
+  loadOrders(): void {
     this.ordersService.getOrders().subscribe((ordenes) => {
       this.orders = ordenes;
+      const id = this.userService.getUserIdFormToken();
+      const userId: string = id !== undefined && id !== null ? id.toString() : '';
 
       // Filtrar las órdenes según su estado
-      this.ordersInProgress = this.orders.filter(order => order.status_id === '1');
-      this.ordersCompleted = this.orders.filter(order => order.status_id === '4');
+      this.ordersInProgress = this.orders.filter(order => order.status === 'Pendiente' && order.client_id === userId);
+      this.ordersCompleted = this.orders.filter(order => order.status === 'Entregado' && order.client_id === userId);
+      this.ordersCanceled = this.orders.filter(order => order.status === 'Cancelado' && order.client_id === userId)
     });
   }
 
